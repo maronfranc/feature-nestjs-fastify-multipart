5ffb939fff5be0f77b04cd7f645f6c5f94f50a32
Máron <maron.franca@gmail.com>
2021-04-26 22:23:12 -0300

    nestjs start

---
+++ sample/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const fastifyAdapter = new FastifyAdapter({
+    logger: true
+  });
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  fastifyAdapter.register(require("fastify-multipart"));
+
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter);
+
+  await app.listen(3000);
+}
+bootstrap();
